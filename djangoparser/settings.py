46085d6035e630a507702122300172d8367b0a14
authoraytee <authoraytee@mail.ru>
2020-12-29 07:41:53 +0700

    F

--- djangoparser/settings.py
+++ djangoparser/settings.py
@@ -109,4 +109,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
