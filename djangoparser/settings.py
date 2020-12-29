e376b75ac698988b9713683e4648842e150eb4fd
authoraytee <authoraytee@mail.ru>
2020-12-29 08:12:33 +0700

    F

--- djangoparser/settings.py
+++ djangoparser/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ub!duq+3=#-*k&ws5srsi!e&^@#4warf+lsd!jsj2=#rf^#4)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,6 +107,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- djangoparser/urls.py
+++ djangoparser/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path, include, re_path
+from djangoparser import settings
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('parser_app.urls')),
 ]
-
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
