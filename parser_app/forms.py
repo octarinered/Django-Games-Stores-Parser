c87190030cfcb5ae1e3a1b0ef4f7d93c72329a00
authoraytee <authoraytee@mail.ru>
2020-12-29 07:36:44 +0700

    new styles

--- parser_app/forms.py
+++ parser_app/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class searchGameForm(forms.Form):
-    gameName = forms.CharField()
+    gameName = forms.CharField(label = "", widget=forms.TextInput(attrs={"placeholder":"Search..."}))
